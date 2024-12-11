884e1fcfdb3a17f2ccaca39d9ff6d0b647cfff2f
SANTHOSH KUMAR PITTA <79613980+santhoshkumar43@users.noreply.github.com>
2024-12-11 10:22:30 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,12 +10,7 @@ export default function Projects() {
         {
             image:pro1,
             title: "Weather Application",
-            sentences: [
-                "santhosh kumar",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh lorem, consequat vel pellentesque quis, varius vitae odio.",
-                "Vivamus pretium libero tortor, eget iaculis ex fermentum nec. Nulla leo ligula, aliquet ac suscipit ut, mollis vitae tellus. Mauris iaculis neque",
-                "Aliquam eget arcu dictum, vulputate elit ac, mollis eros. Maecenas eget iaculis velit. Duis nunc urna, semper sed nunc sed, placerat pellentesque leo. ",
-                "Aliquam eget arcu dictum, vulputate elit ac, mollis eros. Maecenas eget iaculis velit. Duis nunc urna, semper sed nunc sed, placerat pellentesque leo. ",
+            sentences: ["The Weather Application is a modern, React JS-based platform designed to provide users with accurate and real-time weather information. Built using a powerful Weather API, the application delivers dynamic weather updates and forecasts in a sleek, responsive interface."
             ],
             link: "https://santhosh-weather-app.netlify.app/",
         },{
